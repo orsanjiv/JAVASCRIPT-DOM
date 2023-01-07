13e61cbb463ac0edda084fa637c76514d2db06f5
SANJIV GUPTA <90419613+SANJIVGUPTA07@users.noreply.github.com>
2023-01-07 22:02:22 +0530

    Update app1.js

--- DOM Traverse/app1.js
+++ DOM Traverse/app1.js
@@ -1,3 +1,12 @@
 let newLi = document.createElement('li');
 
+newLi.className = 'list-items';
+
+// adding content to new list 
+let conNewLi = document.createTextNode('PUBG IS BEST');
+newLi.append(conNewLi);
+
+// adding this list item in html code 
+
+
 console.log(newLi)
