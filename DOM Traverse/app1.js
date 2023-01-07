0eaf6a25facce2a8c458a4bcebf6292eacb18e98
SANJIV GUPTA <90419613+SANJIVGUPTA07@users.noreply.github.com>
2023-01-07 22:37:06 +0530

    Update app1.js

--- DOM Traverse/app1.js
+++ DOM Traverse/app1.js
@@ -1,12 +1,20 @@
 let newLi = document.createElement('li');
 
 newLi.className = 'list-items';
+newLi.id = 'unique-li';
+// newLi.title/atrribute..  
 
 // adding content to new list 
-let conNewLi = document.createTextNode('PUBG IS BEST');
-newLi.append(conNewLi);
+// let conNewLi = document.createTextNode('PUBG IS BEST');
+// newLi.append(conNewLi); or
+
+newLi.innerHTML = 'PUBGM IS BEST';
 
 // adding this list item in html code 
+let ul = document.querySelector('ul');
+ul.appendChild(newLi)
+
+
 
 
 console.log(newLi)
